e84bf87dbd682fb5118fdc58a4255cc66265c7f4
Captain Ahab <captainahab337@gmail.com>
2021-05-09 13:05:50 -0600

    mysql code updated, index js code in progress

---
+++ department.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS departments_DB;
+
+CREATE DATABASE departments_DB;
+
+USE departments_DB;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(25) NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT(10),
+    FOREIGN KEY (department_id) REFERENCES departments (id) ON DELETE CASCADE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(15),
+    last_name VARCHAR(20),
+    manager_id INT(20),
+    FOREIGN KEY (manager_id) REFERENCES employees (id) ON DELETE SET NULL,
+    role_id INT(10),
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE,
+    PRIMARY KEY(id)
+);
+
+
+INSERT INTO departments (name)
+VALUES ('retail'), ('shipping'), ('orders'), ('support');
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('cashier',20000,1), ('driver',34000,2), ('data_entry',26000,3), ('service',28000,4);
+
+
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES ('Jim','Smith',NULL,1), ('Lisa','Jackson',1,1),('Jack','Vorhees',1,2);
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -11,11 +12,41 @@ const connection = mysql.createConnection({
 
     //updating with MySQL password
     password: 'ohcaptain37#',
-    database: 'departments'
+    database: 'departments_DB'
 });
 
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`);
-    connection.end();
+    runProgram()
 });
+
+function runProgram() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            message: 'what would you like to do?',
+            choices: ['view all employees', 'create employee', 'view all roles', 'create role', 'view all departments', 'create department', 'update employee role']
+        }
+    ])
+    .then(function(answers){
+        switch(answers.option){
+            case 'view all employees':
+                viewEmployees();
+                break;
+            case 'create employee':
+                createEmployee();
+                break;
+            case 'view all departments':
+                deptsAll();
+                break;
+            case 'view all roles':
+                rolesAll();
+                break;
+            case 'search for Employee':
+                employee();
+                break;
+        }
+    })
+}
